1729dd1fd92f90bf7351e3e8a9cf05a0865711b5
Diozefe <diozefe.f.morais@gmail.com>
2018-12-10 07:09:26 -0300

    Atualização de Cor!

--- login/cadastrar.php
+++ login/cadastrar.php
@@ -25,39 +25,46 @@ include("conexao.php");
     <div class="circle-3"></div>
     <div class="inner-part">
       <div class="gif-part">  <form class="speaker-form" method="POST" action="processa.php">
-     <div class="form-row">
-     <label for='full-name'>Nome Completo</label>
-	<input type="text"  name="nome"placeholder="Digite o seu nome completo! ">
-	</div>
+         <div class="form-row">
+             <div class="form-row div-ajust div-esq">
+             <label for='full-name'>Nome Completo</label>
+             <input type="text"  name="nome"placeholder="Digite o seu nome completo! ">
+            </div>
+            <div class="form-row div-ajust div-dir">
+             <label for='email'>E-mail</label>
+             <input type="email"  name="email" placeholder="Digite o seu e-mail!">
+            </div>
+         </div>
 	<div class="form-row">
-	<label for='email'>E-mail</label>
-	<input type="email"  name="email" placeholder="Digite o seu e-mail!">
+        <div class="form-row div-ajust div-esq">
+            <label for='usuario'>Usuário</label>
+            <input id="usuario" type="usuario" name="usuario" placeholder="Digite seu usuario!">
+            </div>
+        <div class="form-row div-ajust div-dir">
+            <label for='full-phone'>Telefone</label>
+            <input type="text"  name="telefone" placeholder="Telefone">
+            </div>
 	</div>
-	<div class="form-row1">
-		<label for='usuario'>Usuário</label>
-	<input type="usuario" name="usuario" placeholder="Digite seu usuario!">
-	</div>
-	<div class="form-row1">
-	<label for='full-phone'>Telefone</label>
-	<input type="text"  name="telefone" placeholder="Telefone">
-	</div>
-	<div class="form-row2">
-	<label for='date'>Data nascimento</label>
-	<input type="date" name="datanascimento">
-	</div>
-
-	
-	<div class="form-row2">
-		<label for='senha'>Senha</label>
-	<input type="password" name="senha" placeholder="Digite sua senha!">
-	</div>
-	<div class="form-row3">
-		<label for='csenha'>Confirme</label>
-	<input type="password" name="csenha" placeholder="Confirmar senha!">
+    <div class="form-row">
+	    <div class="form-row div-esq div-ajust">
+	        <label for='date'>Data nascimento</label>
+	        <input id="date" type="date" name="datanascimento">
+	    </div>
+    </div>
+	<div class="form-row">
+        <div class="form-row div-ajust div-esq">
+            <label for='senha'>Senha</label>
+            <input type="password" name="senha" placeholder="Digite sua senha!">
+        </div>
+        <div class="form-row div-ajust div-dir">
+            <label for='csenha'>Confirme</label>
+            <input type="password" name="csenha" placeholder="Confirmar senha!">
+        </div>
 	</div>
 
 	<input class="button" type="submit" value="CADASTRAR">
-</form></div>
+    </form>
+      </div>
     </div>
     <div class="part-1"></div>
     <div class="part-2"></div>
